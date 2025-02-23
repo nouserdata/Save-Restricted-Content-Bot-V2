209631a0afbbb562b4f920f88de0c6e3d9d6fa38
nouserdata <govindkumary2066@gmail.com>
2025-02-23 16:06:08 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -40,10 +40,10 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
+👨‍💻 Author: chhoro hame hi maan lo
+🌐 GitHub: github ko gaar maaro
 📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+▶️ YouTube: nhi bna sakta
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
